84795c43e8afc47ac228d21d3d4f37ae974d4f17
Yaba Louo Jean de DIEU <gauisyaba@gmail.com>
2026-02-10 22:40:07 +0000

    fix ui bug: footer padding ui

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,29 +10,29 @@ export default function Footer() {
     return (
         <footer className="text-[1rem]">
             <div className="container">
-                <div className="md:flex justify-between md:items-center footer__content">
-                    <div>
-                        <div className="footer__contact">
-                            <a href="#" className="footer__logo">
-                                <img src={Logo} alt="footer logo" />
-                                <span>Elias</span>
+                <div className="mb-8 md:flex justify-between md:items-center">
+                    <div className='mb-4 md:mb-0'>
+                        <div className="flex mb-4 gap-6">
+                            <a href="#" className="flex items-center gap-[0.563rem]">
+                                <img className='w-4 h-4' src={Logo} alt="footer logo" />
+                                <span className='text-white font-(--font-medium)'>Elias</span>
                             </a>
                             <span>gauisyaba@gmail.com</span>
                         </div>
-                        <div className="footer__description">
-                            <p>{t('footer.job_title')}</p>
+                        <div className="text-white">
+                            <p className='leading-[1.4]'>{t('footer.job_title')}</p>
                         </div>
                     </div>
-                    <div className="footer__media">
-                        <h4>Media</h4>
-                        <div className="footer__media__icon">
-                            <a href="https://github.com/victoire20" target="_blank"><img src={Icons.github} alt="github icon" /></a>
-                            <a href="#"><img src={Icons.dribble} alt="dribble icon" /></a>
-                            <a href="#"><img src={Icons.discord} alt="discord icon" /></a>
+                    <div className="flex flex-col items-center mt-0 md:mt-4 footer__media">
+                        <h4 className='mb-75 font-(--font-medium) f-h4 text-white'>Media</h4>
+                        <div className="flex gap-[0.688rem]">
+                            <a className='hover:scale-[1.3]' href="https://github.com/victoire20" target="_blank"><img className='opacity-70 hover:opacity-100 w-5 h-5' src={Icons.github} alt="github icon" /></a>
+                            <a className='hover:scale-[1.3]' href="#"><img className='opacity-70 hover:opacity-100 w-5 h-5' src={Icons.dribble} alt="dribble icon" /></a>
+                            <a className='hover:scale-[1.3]' href="#"><img className='opacity-70 hover:opacity-100 w-5 h-5' src={Icons.discord} alt="discord icon" /></a>
                         </div>
                     </div>
                 </div>
-                <p className="copyright">
+                <p className="text-xs leading-[0.938rem] text-center copyright">
                     &copy;{new Date().getFullYear()}. {t('footer.first_segment_content')}<a href="https://github.com/victoire20">Yaba J</a>.{t('footer.second_segment_content')}<a href="https://www.figma.com/@elias_dev">Elias</a>.
                 </p>
             </div>
